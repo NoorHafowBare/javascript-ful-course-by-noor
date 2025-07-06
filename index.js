3c39e916df834389adf43e9bdca80a6eae7b6090
NoorHafowBare <noorhafowbare@gmail.com>
2025-07-06 19:41:14 +0300

    we learnt loops

--- index.js
+++ index.js
@@ -1,95 +1,36 @@
-// let x = 10;
-// let y = 20;
+// LOOPS
+// console.log("heelo world");
+// console.log("heelo world");
+// console.log("heelo world");
+// console.log("heelo world");
+// console.log("heelo world");
+// console.log("heelo world");
 
-// console.log(x + y);
-// console.log(x - y); // Output:  -10
-// console.log(x * y); // Output: 200
-// console.log(x / y); // Output: 0.5
-// console.log(x % y); // Output: 10
+// Types Of LOOPS
+//1. For LOOP
+//2. While LOOP
+//3. Do While Loop
 
+//1. For LOOP
+// for (let i = 50; i <= 100; i++){
+//     console.log(i);
+// }
 
-// console.log(6 + 4);
+// 2. While Loop
+// let xisaab = 1;
+// while(xisaab <= 10){
+//     console.log(xisaab);
+//     xisaab++;
+// }
 
-// console.log (5 == '5');  // true (loose equality)
-// console.log (5 === '5'); // false (strict equality)
-
-// console.log (5 != 6);    // true
-
-
-// logaical operators
-//AND all conditions must be true
-// let age = 25;
-// let isStudent = true;
-
-// console.log(age > 18 && isStudent); 
-
-
-// //OR at least one condition must be true
-// let hasID = true;
-// let knowsPassword = false;
-
-// console.log(hasID || knowsPassword);
-
-// //NOT negates the condition
-// let isAdmin = false;
-// console.log(!isAdmin);
+//3. Do While Loop
+// let i =10;
+// do{
+//     console.log(i);
+//     i++
+// }while(i <=20);
 
 
-// Control Flow
-// if Statement => runs the code if the condition is True
-
-let age = 18;
-
-if (age >= 18){
-    console.log("you can vote");
-}
-
-// if and else Statement
-// runs one block if true, another block is false
-
-let isRaining = true;
-
-if (isRaining){
-    console.log("take an unbrella");
-}else {
-    console.log("no need to take an unbrella");
-}
-
-// else if Statement
-// checks meltiple conditions
-
-let score = 100;
-
-if (score >= 90){
-    console.log("Grade A");
-}else if (score >= 80){
-    console.log("Grade B");
-}else if (score >= 70){
-    console.log("Grade C");
-}else{
-    console.log("fail");
-}
-
-// Nested if statement
-// putting one if statement
-let age_2 = 17;
-let hasID = false;
-
-if (age_2 >= 18){
-    if (hasID){
-        console.log("you can enter the club");
-    }else{
-        console.log(" you need to have ID");
-    }
-}else {
-    console.log("you are too young");
-}
-
-// logical operators
-
-let isStudent = true;
-let isMember = false;
-
-if (isStudent || isMember){
-    console.log("you get adiscount");
+for (let love = 1; love <= 100; love++){
+    console.log(love, "i love you ❤️");
 }
